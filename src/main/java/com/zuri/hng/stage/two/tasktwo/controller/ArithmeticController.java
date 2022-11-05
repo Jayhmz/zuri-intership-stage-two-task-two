ffc5b625f23ce75ece1f9d4272c44600991207cc
Jayhmz <Jayhmz10@gmail.com>
2022-11-05 14:11:44 +0100

    Update Arithmetic Controller with @crossorigin

--- src/main/java/com/zuri/hng/stage/two/tasktwo/controller/ArithmeticController.java
+++ src/main/java/com/zuri/hng/stage/two/tasktwo/controller/ArithmeticController.java
@@ -3,6 +3,7 @@ package com.zuri.hng.stage.two.tasktwo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import com.zuri.hng.stage.two.tasktwo.model.ResponseModel;
 import com.zuri.hng.stage.two.tasktwo.service.ArithmeticService;
 
 @RestController
+@CrossOrigin("*")
 public class ArithmeticController {
 
 	@Autowired
