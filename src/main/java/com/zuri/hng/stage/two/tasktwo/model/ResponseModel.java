c23abfbee70cba9758a3a1f080941fe7f5404dac
Jayhmz <Jayhmz10@gmail.com>
2022-11-05 15:16:51 +0100

    Update ResponseModel,java

--- src/main/java/com/zuri/hng/stage/two/tasktwo/model/ResponseModel.java
+++ src/main/java/com/zuri/hng/stage/two/tasktwo/model/ResponseModel.java
@@ -2,15 +2,19 @@ package com.zuri.hng.stage.two.tasktwo.model;
 
 import javax.validation.constraints.NotNull;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
 public class ResponseModel {
 	
 	private String slackUsername;
-	@NotNull
+	@NotNull(message = "please provide an operation type")
+	@JsonProperty(required = true)
 	private String operation_type;
 	
 	@NotNull
+	@JsonProperty(required = true)
 	private int result;
 }
